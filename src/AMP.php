c3689e3306a5927f03238fd89a7ae893484f863e
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2016-01-28 18:27:42 +0530

    Very initial stuff; creating a class so it can be called from the AMP Service in Drupal

---
+++ src/AMP.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lullabot\AMP;
+
+
+class AMP {
+
+  /**
+   * AMP constructor.
+   */
+  public function __construct() {
+  }
+
+  /**
+   * Convert an HTML Fragment to AMP HTML
+   *
+   * @param $html
+   * @return string
+   */
+  public function convertToAMP($html) {
+    // We're not doing anything at the moment!
+    return $html;
+  }
+}
