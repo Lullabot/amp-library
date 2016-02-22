483ab5021e7bdfb4c7288d4b25c432a7e58eefb0
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2016-02-22 20:22:00 +0530

    change the description text when tags and attributes are removed

--- src/ActionTakenType.php
+++ src/ActionTakenType.php
@@ -6,8 +6,8 @@ use MyCLabs\Enum\Enum;
 
 class ActionTakenType extends Enum
 {
-    const TAG_REMOVED = 'tag was removed.';
-    const ATTRIBUTE_REMOVED = 'attribute was removed.';
+    const TAG_REMOVED = 'tag was removed due to validation issues.';
+    const ATTRIBUTE_REMOVED = 'attribute was removed due to validation issues.';
     const IMG_CONVERTED = 'tag was converted to the amp-img tag.';
     const IFRAME_CONVERTED = 'tag was converted to the amp-iframe tag.';
 }
