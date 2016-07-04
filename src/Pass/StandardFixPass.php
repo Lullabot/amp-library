be89db2f61f70804c04ea67ef8f59914140c51ef
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2016-07-04 16:59:01 +0530

    Remove outdated comment

--- src/Pass/StandardFixPass.php
+++ src/Pass/StandardFixPass.php
@@ -43,8 +43,8 @@ class StandardFixPass extends BasePass
         ValidationErrorCode::MISSING_URL,
         ValidationErrorCode::UNESCAPED_TEMPLATE_IN_ATTR_VALUE,
         ValidationErrorCode::TEMPLATE_PARTIAL_IN_ATTR_VALUE
-        // @todo ValidationErrorCode::MUTUALLY_EXCLUSIVE_ATTRS?
     ];
+    
     protected $remove_tags_for_codes = [
         ValidationErrorCode::WRONG_PARENT_TAG,
         ValidationErrorCode::DISALLOWED_TAG,
