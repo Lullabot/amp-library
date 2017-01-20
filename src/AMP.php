2a61d3695d8f7cda901218b52868d5d0e6ca39cc
Olena Beyer <obeyer@sugarinc.com>
2017-01-19 18:16:07 -0800

    override FB embeds url validation

--- src/AMP.php
+++ src/AMP.php
@@ -63,7 +63,7 @@ class AMP
         'Lullabot\AMP\Pass\IframeTagTransformPass',
         'Lullabot\AMP\Pass\InstagramTransformPass',
         'Lullabot\AMP\Pass\PinterestTagTransformPass',
-        'Lullabot\AMP\Pass\FacebookNonIframeTransformPass',
+        'Lullabot\AMP\Pass\SugarFacebookNonIframeTransformPass',
         'Lullabot\AMP\Pass\TwitterTransformPass',
         'Lullabot\AMP\Pass\StandardScanPass',
         'Lullabot\AMP\Pass\SugarStandardFixPass',
