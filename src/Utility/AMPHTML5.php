b8f7d2ccbdf13eed5658d4306e5eb59b98f147bb
TJ <temi_jegede@hotmail.co.uk>
2018-11-21 11:30:25 +0000

    remove  calls to deprecated inputStream

--- src/Utility/AMPHTML5.php
+++ src/Utility/AMPHTML5.php
@@ -18,7 +18,6 @@
 namespace Lullabot\AMP\Utility;
 
 use Masterminds\HTML5;
-use Masterminds\HTML5\Parser\InputStream;
 
 /**
  * Class AMPHTML5
@@ -39,11 +38,11 @@ class AMPHTML5 extends HTML5
      * Similar to \Masterminds\HTML5::parse() method in superclass but we use our custom (sub-classed) tokenizer and DOM tree
      * builder to achieve desired effect of adding a line number attribute to each tag of the output DOM document.
      *
-     * @param InputStream $inputstream
+     * @param string $inputstream
      * @param array $options
      * @return \DOMDocument
      */
-    public function parse(InputStream $inputstream, array $options = [])
+    public function parse($inputstream, array $options = [])
     {
         // User options override default options in $this->options
         $final_options = array_merge($this->options, $options);
